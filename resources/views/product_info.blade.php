f6ce775ae14ab59c50e177d4808576c8ba46a817
Dragon <dragon@DragondeMacBook-Pro.local>
2022-01-24 18:11:40 +0800

    对接index和product-info页面

--- resources/views/product_info.blade.php
+++ resources/views/product_info.blade.php
@@ -12,52 +12,35 @@
 
     <div class="product-info-box">{!! $product->description !!}</div>
 
-    <div class="product-info-file-box">点击下载 <a href="{{URL::asset('uploads').'/'.$product->file}}">{{$product->title}}</a> 相关资料</div>
+    <div class="product-info-file-box">
+        点击下载
+        <a href="{{URL::asset('uploads').'/'.$product->file}}">{{$product->title}}</a>
+        相关资料
+    </div>
 
     <div class="more-product-box">
         <div class="more-product-box-title">想知道我们更多产品消息？</div>
-
         <div class="more-product-box-list-box swiper-container">
             <div class="swiper-wrapper">
-
                 @foreach($products as $product)
-                    <div class="more-product-box-list-box-item swiper-slide" style='background: url("{{URL::asset('uploads').'/'.$product->image}}"); background-size: 100% 100%; background-repeat: no-repeat;'>
+                    <div class="more-product-box-list-box-item swiper-slide"
+                         style='background: url("{{URL::asset('uploads').'/'.$product->image}}"); background-size: 100% 100%; background-repeat: no-repeat;'>
                         <div class="more-product-box-list-box-item-mask"></div>
-                        <a href="/product-info/{{$product->id}}"><div class="more-product-box-list-box-item-button font-color-white text-overflow">{{$product->title}}</div></a>
+                        <a href="/product-info/{{$product->id}}">
+                            <div
+                                class="more-product-box-list-box-item-button font-color-white text-overflow">{{$product->title}}</div>
+                        </a>
                     </div>
                 @endforeach
-
-{{--                <div class="more-product-box-list-box-item swiper-slide" style='background: url("{{URL::asset('images/static/project.jpg')}}"); background-size: 100% 100%; background-repeat: no-repeat;'>--}}
-{{--                    <div class="more-product-box-list-box-item-mask"></div>--}}
-{{--                    <div class="more-product-box-list-box-item-button font-color-white">广播通讯</div>--}}
-{{--                </div>--}}
-{{--                <div class="more-product-box-list-box-item swiper-slide" style='background: url("{{URL::asset('images/static/project.jpg')}}"); background-size: 100% 100%; background-repeat: no-repeat;'>--}}
-{{--                    <div class="more-product-box-list-box-item-mask"></div>--}}
-{{--                    <div class="more-product-box-list-box-item-button font-color-white">广播通讯</div>--}}
-{{--                </div>--}}
-{{--                <div class="more-product-box-list-box-item swiper-slide" style='background: url("{{URL::asset('images/static/project.jpg')}}"); background-size: 100% 100%; background-repeat: no-repeat;'>--}}
-{{--                    <div class="more-product-box-list-box-item-mask"></div>--}}
-{{--                    <div class="more-product-box-list-box-item-button font-color-white">广播通讯</div>--}}
-{{--                </div>--}}
-{{--                <div class="more-product-box-list-box-item swiper-slide" style='background: url("{{URL::asset('images/static/project.jpg')}}"); background-size: 100% 100%; background-repeat: no-repeat;'>--}}
-{{--                    <div class="more-product-box-list-box-item-mask"></div>--}}
-{{--                    <div class="more-product-box-list-box-item-button font-color-white">广播通讯</div>--}}
-{{--                </div>--}}
-{{--                <div class="more-product-box-list-box-item swiper-slide" style='background: url("{{URL::asset('images/static/project.jpg')}}"); background-size: 100% 100%; background-repeat: no-repeat;'>--}}
-{{--                    <div class="more-product-box-list-box-item-mask"></div>--}}
-{{--                    <div class="more-product-box-list-box-item-button font-color-white">广播通讯</div>--}}
-{{--                </div>--}}
             </div>
         </div>
         <div class="arrow-box">
             <div class="pagination"></div>
         </div>
-
     </div>
 
     <script>
         var _swiper = new Swiper('.swiper-container', {
-            // loop: true,
             autoplay: 3000,
             slidesPerView: 4,
             slidesPerGroup: 1,
