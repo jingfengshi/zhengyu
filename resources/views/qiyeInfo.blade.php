575ef08c00f625bc1d2604818133e73c98cd9bb8
jasperjing <240023810@qq.com>
2022-02-05 10:12:03 +0800

    隐私声明和企业信息

--- resources/views/qiyeInfo.blade.php
+++ resources/views/qiyeInfo.blade.php
@@ -1,6 +1,7 @@
 @extends("main")
 
 @section("content")
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('css/private.css')}}">
     <style>
         div font{
             font-size: 24px;
@@ -13,30 +14,29 @@
             font-style: normal;
         }
     </style>
-    <div style="background-color: rgba(242, 242, 242, 1);height: 166px">
-        <div style="margin-top: 1rem;padding-top: 4rem;padding-left: 19rem;" >
-            <div style="width: 144px;height: 50px; font-size: 36px;font-weight: 700;color: #555555;font-style: normal;">
+    <div class="private-title">
+        <div class="private-title-big">
+            <div class="private-title-big-font">
                 重要信息
             </div>
-            <div style="width: 300px;height: 25px;margin-top:1rem;font-weight: 200;
-    font-style: normal;
-    font-size: 18px;
-    color: #AAAAAA;">
+            <div class="private-title-small-font">
                 更新日期：{{$content->updated_at->format('Y').'年'.$content->updated_at->format('m').'月'.$content->updated_at->format('d').'日'}}
             </div>
         </div>
     </div>
 
 
-    <div style="margin-top: 1rem;padding-top: 4rem;padding-left: 19rem;   font-family: '苹方 细体', '苹方 中等', '苹方', sans-serif;
-    font-weight: 300;
-    font-style: normal;
-    font-size: 2rem;
-    line-height: 48px;" class="container">
+    <div style="height: auto;width: 1920px;margin: auto">
+        <div style="margin-top: 1rem;padding-top: 4rem;padding-left: 19rem;
+        font-weight: 300;
+        font-style: normal;
+        font-size: 2rem;
+        line-height: 48px;" class="container">
 
-        {!! $content->value !!}
+            {!! $content->value !!}
 
 
+        </div>
     </div>
 
 
