7cf0a6cfea00a80fcbaff0a454b9a7275394de8e
jasperjing <240023810@qq.com>
2022-02-05 11:02:48 +0800

    新闻页调整

--- resources/views/newsList.blade.php
+++ resources/views/newsList.blade.php
@@ -5,48 +5,64 @@
 
     <link rel="stylesheet" type="text/css" href="css/cases.css">
 
-    <div class="cases-top-title">
-        <a href="">伙伴</a>
-        <a href="">共同推动行业前进</a>
-        <a href="">服务的品牌</a>
-        <a href="">案例</a>
-    </div>
+    <div style="width: 1920px;height: auto;margin: auto">
+        <div class="cases-top-title">
+            <a href="">关于正域</a>
+            <a href="">新闻</a>
+            <a href="">企业荣誉</a>
+            <a href="">产品认证</a>
+            <a href="">联系我们</a>
+        </div>
 
-    <div class="cases-banner">
-        <img src="{{URL::asset('images/服务的品牌/u4314.jpg')}}">
-    </div>
+        <div class="cases-banner">
+            <img src="{{URL::asset('images/xinwen.jpg')}}">
+        </div>
 
 
-    <div class="cases-title">
-        新闻活动
-    </div>
-    <div class="grid grid-cols-2 justify-items-center" style="margin-left: 12rem;margin-top: 2rem">
+        <div class="cases-title">
+            新闻活动
+        </div>
+        <div class="grid grid-cols-2 justify-items-center" style="margin:auto;margin-top: 4rem;width: 1500px;height: auto">
 
-        @foreach($newsList as $news)
-            <div class="flex w-full min-h-[17rem]" style="margin-right: 2rem">
-                <div class="min-h-[12rem]  w-2/5" >
-                    <img src="uploads/{{$news->cover}}" alt="" style="width: 100%;height: 100%;">
+            @foreach($newsList as $news)
+                <div class="flex w-full min-h-[17rem]" style="margin-right: 2rem;width: 708px;height: 256px;">
+                        <img src="uploads/{{$news->cover}}" alt="" style="width: 255px;height:200px;">
+                    <div class="min-h-[12rem] bg-black-100 " style="margin-left:1rem;width: 50%">
+                        <div style="width:380px;height: 56px;font-family: 'Arial Normal', 'Arial', sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 18px;
+    line-height: 28px;">
+                            {{$news->title}}
+                        </div>
+                        <div  style="width: 375px;height: 77px;
+font-weight: 200;
+    font-style: normal;
+    text-align: left;    font-size: 15px;
+    line-height: 28px;">
+                            {{$news->desc}}
+                        </div>
+                        <div class="h-1/3  w-full" style="text-align: right;font-size: 1rem;font-weight: 200;">
+                            <a href="{{route('news.show',[$news->id])}}" style="position: relative;right: 0;bottom: -4rem;font-weight: 400;
+    font-style: normal;
+    font-size: 12px;
+    color: rgba(111, 111, 111, 0.996078431372549);
+    text-align: right;
+    line-height: 28px;">>更多详情</a>
+                        </div>
+                    </div>
+
+
                 </div>
-                <div class="min-h-[12rem] bg-black-100 " style="margin-left: 1rem;width: 50%">
-                    <div class="h-1/3  w-full " style="font-size: 1.2rem;font-weight: 400;">
-                       {{$news->title}}
-                    </div>
-                    <div class="h-1/3  w-full" style="font-size: 1rem;line-height: 1.5rem;font-weight: 200;">
-                       {{$news->desc}}
-                    </div>
-                    <div class="h-1/3  w-full" style="text-align: right;font-size: 1rem;font-weight: 200;">
-                        <a href="{{route('news.show',[$news->id])}}" style="position: relative;right: 0;bottom: -4rem">>更多详情</a>
-                    </div>
-                </div>
-
-
-            </div>
-        @endforeach
+            @endforeach
 
 
 
 
+        </div>
     </div>
+
+
 
 
 
