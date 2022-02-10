7ebe6b95f064860517c3d200867db98864634ad0
Dragon <dragon@DragondeMacBook-Pro.local>
2022-02-10 10:49:17 +0800

    统一右上角四个链接页面的导航样式

--- resources/views/gongyingshang.blade.php
+++ resources/views/gongyingshang.blade.php
@@ -4,7 +4,26 @@
 
     <link rel="stylesheet" type="text/css" href="{{URL::asset('css/brand.css')}}">
     <div style="width: 1920px;height: auto;margin: auto">
-        @include('_bread_case')
+        <style>
+            .cases-top-title{
+                padding: 2.8rem 240px 1.45rem 240px;
+            }
+
+            .cases-top-title a{
+                margin-right: 2.4rem;
+                font-family: 'Arial Normal', 'Arial', sans-serif;
+                font-weight: 400;
+                font-style: normal;
+                font-size: 16px;
+                color: #484848;
+            }
+        </style>
+        <div class="cases-top-title">
+            <a href="{{route('supply')}}">伙伴</a>
+            <a href="{{route('supply')}}#foward">共同推动行业前进</a>
+            <a href="{{route('brand')}}">服务的品牌</a>
+            <a href="{{route('cases')}}">案例</a>
+        </div>
 
         <div class="brand-banner">
             <img src="{{URL::asset('images/gongyingshang.jpg')}}">
